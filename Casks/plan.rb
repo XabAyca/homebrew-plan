82773d3ba10bbfe729c90cb636b696813c5f392d
Xabi Aycaguer <80123816+XabAyca@users.noreply.github.com>
2026-09-14 10:42:35 +0200

    Bump plan cask to 0.1.4

--- Casks/plan.rb
+++ Casks/plan.rb
@@ -1,14 +1,14 @@
 cask "plan" do
-  version "0.1.0"
+  version "0.1.4"
 
   on_arm do
-    sha256 "REPLACE_WITH_SHA256_OF_AARCH64_DMG"
+    sha256 "3f962f35f73df86543fc85848f427d8f3a6e42936d81bb7d1a63ee62855fbca7"
 
     url "https://github.com/XabAyca/plan-releases/releases/download/v#{version}/plan-#{version}-aarch64-apple-darwin.dmg"
   end
 
   on_intel do
-    sha256 "REPLACE_WITH_SHA256_OF_X86_64_DMG"
+    sha256 "29d2cc8be5cedeb45e4cd42771f22427a02d4791648371405a007790b0add685"
 
     url "https://github.com/XabAyca/plan-releases/releases/download/v#{version}/plan-#{version}-x86_64-apple-darwin.dmg"
   end
